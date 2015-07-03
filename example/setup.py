7af69538a9f417d2c90b53b49e0c4fd3e59b1ee8
Dmitry <root@uralbash.ru>
2015-07-03 20:23:17 +0500

    clean example

--- example/setup.py
+++ example/setup.py
@@ -1,11 +1,21 @@
+import os
+
 from setuptools import setup
 
 version = '1.0'
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+
+def read(name):
+    with open(os.path.join(here, name)) as f:
+        return f.read()
 
 setup(
     name='ps_tree_example',
     version=version,
     py_modules=['ps_tree_example'],
+    install_requires=read('requirements.txt'),
     entry_points="""
 [paste.app_factory]
 main = ps_tree_example:main
